60bf92832ee16ee40308aa3d1e819a6beb500b24
unknown <ajayjagtap.10@gmail.com>
2021-08-18 20:25:14 +0530

    Uploading Project

---
+++ projects/ngx-bs-modal/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-bs-model
+ */
+
+export * from './lib/dialog.service';
+export * from './lib/dialog.component';
+export * from './lib/bootstrap-modal.module';
---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { DialogComponent, DialogService } from 'ngx-bs-modal';
+
+export interface AlertModel {
+  title?: string;
+  message?: string;
+}
+
+@Component({
+  selector: 'alert',
+  template: `<div class="modal-dialog">
+                <div class="modal-content">
+                   <div class="modal-header">
+                   <h4 class="modal-title">{{title || 'Alert!'}}</h4>
+                   <button type="button" class="close" (click)="close()" >&times;</button>
+                   </div>
+                   <div class="modal-body">
+                     <p>{{message || 'TADAA-AM!'}}</p>
+                   </div>
+                   <div class="modal-footer">
+                     <button type="button" class="btn btn-primary" (click)="close()">OK</button>
+                   </div>
+                </div>
+             </div>`
+})
+export class AlertComponent extends DialogComponent<AlertModel, null> implements AlertModel {
+  title: string;
+  message: string;
+  constructor(dialogService: DialogService) {
+    super(dialogService);
+  }
+}
